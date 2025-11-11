9d8c94ec5195a9676a779dbfa0e8316153d066bf
turklievvv <turklievvv1@gmail.com>
2025-11-11 08:19:42 +0300

    Обновляю все файлы

--- app/src/main/java/com/example/myorionapplication/SmsActivity.kt
+++ app/src/main/java/com/example/myorionapplication/SmsActivity.kt
@@ -3,6 +3,8 @@ package com.example.myorionapplication
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -11,12 +13,8 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.isVisible
 import androidx.core.widget.doOnTextChanged
-import androidx.lifecycle.lifecycleScope
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.textfield.TextInputEditText
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 
 class SmsActivity : AppCompatActivity() {
 
@@ -24,28 +22,26 @@ class SmsActivity : AppCompatActivity() {
     private lateinit var resendButton: Button
     private lateinit var number: TextView
     private lateinit var codeVerification: TextInputEditText
-    private lateinit var backButton: MaterialButton
+
     private var phoneNumber: String = ""
-    private var timerJob: Job? = null
+    private var countDownTimer: CountDownTimer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_sms)
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        phoneNumber = intent.getStringExtra(PHONE_NUMBER).orEmpty()
+        phoneNumber = intent.getStringExtra(PHONE_NUMBER) ?: ""
 
         timerText = findViewById(R.id.timerText)
         resendButton = findViewById(R.id.resendButton)
         number = findViewById(R.id.number)
         codeVerification = findViewById(R.id.phoneEditText)
-        backButton = findViewById<MaterialButton>(R.id.backButton)
 
         number.text = "+7$phoneNumber"
 
@@ -53,11 +49,10 @@ class SmsActivity : AppCompatActivity() {
 
         resendButton.setOnClickListener {
             resendButton.isVisible = false
-            timerJob?.cancel()
             startCountdown()
         }
 
-        backButton.setOnClickListener {
+        findViewById<MaterialButton>(R.id.backButton).setOnClickListener {
             finish()
         }
 
@@ -70,27 +65,25 @@ class SmsActivity : AppCompatActivity() {
     }
 
     private fun startCountdown() {
-        timerJob =
-            lifecycleScope.startCountDownTimer(startTime = 60_000L, period = 1000L, start = { _ ->
-                withContext(Dispatchers.Main) {
-                    resendButton.isVisible = false
-                    timerText.isVisible = true
-                }
-            }, tick = { time ->
-                withContext(Dispatchers.Main) {
-                    val seconds = time / 1000
-                    timerText.text = "Отправить повторно через ${seconds}s"
-                }
-            }, end = { _ ->
-                withContext(Dispatchers.Main) {
-                    resendButton.isVisible = true
-                    timerText.isVisible = false
-                }
-            })
+        countDownTimer?.cancel()
+
+        val totalMillis = 30_000L // 30 секунд
+
+        countDownTimer = object : CountDownTimer(totalMillis, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                val secondsLeft = millisUntilFinished / 1000
+                timerText.text = getString(R.string.timerText,secondsLeft)
+            }
+
+            override fun onFinish() {
+                timerText.text = ""
+                resendButton.isVisible = true
+            }
+        }.start()
     }
 
     override fun onDestroy() {
-        timerJob?.cancel()
+        countDownTimer?.cancel()
         super.onDestroy()
     }
 
